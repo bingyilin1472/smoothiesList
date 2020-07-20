0a5321e3a0d923302ef3fdfb2e12196cc30117e9
Dante <dabte_lin@akousist.com>
2020-07-20 15:49:50 +0800

    Navbar development

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended pink darken-4">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Smoothies</span>
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
